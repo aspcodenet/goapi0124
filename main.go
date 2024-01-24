a309121114c73fd98d8f4615f5a2fb3a91bc67bf
aspcodenet <stefan.holmberg@systementor.se>
2024-01-24 18:23:41 +0100

    a

--- main.go
+++ main.go
@@ -1,21 +1,52 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"strconv"
 
+	"github.com/gin-gonic/gin"
 	"systementor.se/goapi0124/data"
 )
 
-func main() {
-	emp := data.Employee{
-		Id:   1,
-		Age:  12,
-		City: "Test",
-		Namn: "Oliver",
-	}
-	//	salary := data.CalculateSalary(emp)
-	salary := emp.CalculateSalary()
+var employees []data.Employee
 
-	//emp.CalculateSalary();
-	fmt.Printf("Hello %v %v", emp, salary)
+func apiEmployee(c *gin.Context) {
+	//var employees []data.Employee
+	//data.DB.Find(&employees)
+
+	c.IndentedJSON(http.StatusOK, employees)
+}
+
+func apiEmployeeById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, emp := range employees {
+		i, _ := strconv.Atoi(id)
+		if emp.Id == i {
+			c.JSON(http.StatusOK, emp)
+		}
+	}
+
+	c.String(http.StatusNotFound, "")
+}
+
+func main() {
+	employees = append(employees, data.Employee{
+		Id:   1,
+		Age:  15,
+		City: "Stockholm",
+		Namn: "Oliver",
+	})
+	employees = append(employees, data.Employee{
+		Id:   2,
+		Age:  51,
+		City: "Stockholm",
+		Namn: "Stefam",
+	})
+
+	router := gin.Default()
+	router.GET("/api/employee", apiEmployee)
+	router.GET("/api/employee/:id", apiEmployeeById)
+	//.POST
+	router.Run(":8080")
 }
