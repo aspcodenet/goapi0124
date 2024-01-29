c23f56eb8bd3a06fad1ba5e137af2147e166b2ba
aspcodenet <stefan.holmberg@systementor.se>
2024-01-29 17:03:24 +0100

    v2

--- main.go
+++ main.go
@@ -17,12 +17,56 @@ func apiEmployee(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, employees)
 }
 
+func apiEmployeeNew(c *gin.Context) {
+	// Det kommer in en ny exmployee som JSON
+	// vi ska ta JSON -> employee
+
+	// stoppa in den i slicen
+
+	// try{
+	// 	c.BindJSON(&employee)
+	// 	Dataabsesdfjklasdf(&employee)
+	// }
+	// catch{
+	// 	dfdfsfs
+	// }
+
+	// "nice att slippa exceptions -> exceptions är slow och krävande"
+	var employee data.Employee
+	// err := c.BindJSON(&employee)
+	// if err != nil {
+
+	// }
+
+	if err := c.BindJSON(&employee); err != nil {
+		c.AbortWithStatus(400)
+		return
+	}
+	employee.Id = 123
+	employees = append(employees, employee)
+	c.IndentedJSON(http.StatusCreated, employee)
+}
+
 func apiEmployeeById(c *gin.Context) {
 	id := c.Param("id")
 
 	for _, emp := range employees {
 		i, _ := strconv.Atoi(id)
 		if emp.Id == i {
+			c.JSON(http.StatusOK, emp)
+		}
+	}
+
+	c.String(http.StatusNotFound, "")
+}
+
+func apiEmployeeUpdate(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, emp := range employees {
+		i, _ := strconv.Atoi(id)
+		if emp.Id == i {
+			c.BindJSON(&emp)
 			c.JSON(http.StatusOK, emp)
 		}
 	}
@@ -47,6 +91,10 @@ func main() {
 	router := gin.Default()
 	router.GET("/api/employee", apiEmployee)
 	router.GET("/api/employee/:id", apiEmployeeById)
+
+	// NY!!!
+	router.POST("/api/employee", apiEmployeeNew)
+	router.PUT("/api/employee/:id", apiEmployeeUpdate)
 	//.POST
 	router.Run(":8080")
 }
